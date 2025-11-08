7c0810d99636da2cb4a5a46ed57568057de53ae4
ashgup08 <96999651+ashgup08@users.noreply.github.com>
2025-11-07 23:12:17 -0600

    idk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainAuto.java
@@ -0,0 +1,113 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+import java.util.List;
+
+@Autonomous(name="Main Autonomous",group = "Autonomous")
+public class mainAuto extends LinearOpMode {
+    @Override
+    DcMotor frontLeftDrive;
+    DcMotor backLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor backRightDrive;
+    DcMotor intake;
+    DcMotor outtake;
+    GoBildaPinpointDriver odoComputer;
+    public void runOpMode(){
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "lf");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "lb");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "rf");
+        backRightDrive = hardwareMap.get(DcMotor.class, "rb");
+        intake = hardwareMap.get(DcMotor.class,"Intake");
+        outtake = hardwareMap.get(DcMotor.class,"Outtake");
+
+        //Odo. computer
+        odoComputer = hardwareMap.get(GoBildaPinpointDriver.class, "odoComputer");
+        odoComputer.initialize();
+        odoComputer.resetPosAndIMU();
+
+        telemetry.addLine("===motors and odometry initialized===");
+
+        // ########################################################################################
+        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
+        // ########################################################################################
+        // Most robots need the motors on one side to be reversed to drive forward.
+        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
+        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
+        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
+        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
+        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
+        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        intake.setDirection(DcMotor.Direction.REVERSE);
+        outtake.setDirection(DcMotor.Direction.FORWARD);
+
+
+
+
+        // Wait for the game to start (driver presses START)
+        telemetry.addData("Status", "Initialized");
+
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while(opModeIsActive()){
+            odoComputer.update();
+            List<AprilTagDetection> tags = camera.getDetection();
+            if(!tags.isEmpty(
+
+
+
+
+
+
+
+
+
+
+
+            telemetry.addLine("===Testing odometry===");
+            telemetry.addData("X (mm):",odoComputer.getPosX());
+            telemetry.addData("Y (mm):",odoComputer.getPosY());
+            telemetry.addData("Heading (rad);",odoComputer.getHeading());
+            telemetry.update();
+
+        }
+    }
+    //stopping all motors
+    public void powerAllMotors(double power){
+        frontRightDrive.setPower(power);
+        frontLeftDrive.setPower(power);
+        backRightDrive.setPower(power);
+        backLeftDrive.setPower(power);
+        intake.setPower(power);
+        outtake.setPower(power);
+    }
+
+
+    //driving forward for certain distance
+    public void distanceDrive(double power, double distance){
+        double initalX = odoComputer.getPosX();
+        double initalY = odoComputer.getPosY(); //mm
+        powerAllMotors(power);
+        while(opModeIsActive() && Math.hypot(odoComputer.getPosX()-initalX, odoComputer.getPosY()-initalY)<distance-2){
+            odoComputer.update();
+        }
+        powerAllMotors(0);
+
+    }
+    public void rotate(double degree, double power){
+
+    }
+
+}
