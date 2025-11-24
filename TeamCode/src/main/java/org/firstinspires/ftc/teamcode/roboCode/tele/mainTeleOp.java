a27d8fc884dcc18ee60d2af6784cb0320fad6d5e
Ashwin Gupta <bluejay2008@outlook.com>
2025-11-24 15:49:46 -0600

    Fixed Limelight detection and max range.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roboCode/tele/mainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roboCode/tele/mainTeleOp.java
@@ -1,18 +1,22 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.roboCode.tele;
 
 import com.pedropathing.follower.Follower;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 import java.util.List;
@@ -25,15 +29,24 @@ public class mainTeleOp extends LinearOpMode {
     private ElapsedTime runtime;
     private DcMotor frontLeftDrive, backLeftDrive, frontRightDrive, backRightDrive,
             intake, outtake, transfer;
+    private Servo b1, b2;
+    private boolean aPressed = false;
+    private boolean servopos = false;
 
-    private Servo rBrake, lBrake;
+
     private Limelight3A limeLight;
+    private IMU imu;
     private Follower follower;
 
     private boolean rBumper = false;
     private boolean lBumper = false;
     private double rl = .8;
 
+    double B2U = 0.19;
+    double B2C = 0;
+
+    double B1U = 0.83;
+    double B1C = 1;
     private double adjust = 1;
     private double rPos = 0;
 
@@ -45,7 +58,7 @@ public class mainTeleOp extends LinearOpMode {
     public void runOpMode() {
 
         //<editor-fold desc="INIT">
-        follower = Constants.createFollower(hardwareMap);
+        //follower = Constants.createFollower(hardwareMap);
 
         ElapsedTime runtime = new ElapsedTime();
         //chassis
@@ -58,10 +71,13 @@ public class mainTeleOp extends LinearOpMode {
         outtake = hardwareMap.get(DcMotor.class,"Outtake");
         transfer = hardwareMap.get(DcMotor.class,"Transfer");
         //braking system
-        rBrake = hardwareMap.get(Servo.class,"rb");
-        lBrake = hardwareMap.get(Servo.class,"lb");
+
+        b2 = hardwareMap.get(Servo.class, "ls");
+        b1 = hardwareMap.get(Servo.class, "rs");
         //April Tags
         limeLight = hardwareMap.get(Limelight3A.class,"Limelight");
+        //imu
+        imu = hardwareMap.get(IMU.class,"IMU");
 
         //directions
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -69,9 +85,15 @@ public class mainTeleOp extends LinearOpMode {
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
         intake.setDirection(DcMotor.Direction.REVERSE);
-        outtake.setDirection(DcMotor.Direction.FORWARD);
+        outtake.setDirection(DcMotor.Direction.REVERSE);
         transfer.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        b1.setPosition(B1U);
+        b2.setPosition(B2U);
+        limeLight.setPollRateHz(100);
+        limeLight.pipelineSwitch(1);
         limeLight.start();
+
 
         telemetry.addData("Status", "Initialized");
 
@@ -88,13 +110,14 @@ public class mainTeleOp extends LinearOpMode {
 
             updateAuxiliaryMotors();
 
+            scanAprilTags();
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addLine("===Testing intake and outtake===");
             telemetry.addData("Intake Power:", "%f", intake.getPower());
             telemetry.addData("Outtake Power:","%f", outtake.getPower());
 
-            scanAprilTags();
+            //scanAprilTags();
 
             telemetry.update();
         }
@@ -102,7 +125,7 @@ public class mainTeleOp extends LinearOpMode {
     }
 
     private void updateDrivetrain(){
-        double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
         double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
         double rx = gamepad1.right_stick_x * rl;
 
@@ -115,6 +138,13 @@ public class mainTeleOp extends LinearOpMode {
         double backLeftPower = (y - x + rx) / denominator;
         double frontRightPower = (y - x - rx) / denominator;
         double backRightPower = (y + x - rx) / denominator;
+/*
+        frontLeftPower = gamepad1.dpad_up ? 1.0:0;
+        frontRightPower = gamepad1.dpad_right ? 1.0:0;
+        backRightPower = gamepad1.dpad_down ? 1.0:0;
+        backLeftPower = gamepad1.dpad_left ? 1.0:0;
+*/
+
 
         frontLeftDrive.setPower(frontLeftPower);
         backLeftDrive.setPower(backLeftPower);
@@ -131,13 +161,23 @@ public class mainTeleOp extends LinearOpMode {
         double intakePower = gamepad1.x ? flip :0.0;
         double transferPower = gamepad1.y ? flip :0;
         double outtakePower = gamepad1.b ? adjust:0;
-        double rBrakePosition = 0;
 
-        if(gamepad1.left_bumper&&!lBumper){
-            //activate braking
-            rBrake.setPosition(rBrakePosition);
+        if (gamepad1.a && !aPressed) {
+            servopos = !servopos;
+            aPressed = true;
         }
-        lBumper = gamepad1.left_bumper;
+
+        if (!gamepad1.a) aPressed = false;
+
+        if(servopos){
+            b1.setPosition(B1C);
+            b2.setPosition(B2C);
+        }else {
+            b1.setPosition(B1U);
+            b2.setPosition(B2U);
+        }
+
+
         if(gamepad1.dpad_down)
             adjust-=.05;
         if(gamepad1.dpad_up)
@@ -149,8 +189,30 @@ public class mainTeleOp extends LinearOpMode {
         transfer.setPower(transferPower);
 
     }
+
     private void scanAprilTags(){
+        YawPitchRollAngles ore = imu.getRobotYawPitchRollAngles();
+        limeLight.updateRobotOrientation(ore.getYaw(AngleUnit.DEGREES));
+        //latest limelight result
         LLResult result = limeLight.getLatestResult();
+        if(result!=null && result.isValid()){
+            Pose3D pose = result.getBotpose_MT2();
+            telemetry.addData("Target X (Degrees)",result.getTx());
+            telemetry.addData("Target Y (Degrees)",result.getTy());
+            telemetry.addData("Botpose",pose.toString());
+
+        }
+        else
+            telemetry.addLine("No targets detected from Limelight.");
+
+
+
+    }
+
+}
+
+
+/*        LLResult result = limeLight.getLatestResult();
         if(result!=null&&result.isValid()){
             List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
             if(!fiducials.isEmpty()){
@@ -158,15 +220,17 @@ public class mainTeleOp extends LinearOpMode {
                 int id = tag.getFiducialId();
                 Pose3D pose = tag.getRobotPoseTargetSpace();
                 telemetry.addData("Tag ID: ", id);
-                telemetry.addData("X (Meters): ",pose.getX());
-                telemetry.addData("Y (Meters): ",pose.getY());
-                telemetry.addData("Z (Meters): ", pose.getZ());
-                telemetry.addData("Yaw (Degrees): ", Math.toDegrees(pose.getRotation().getYaw()));
+                telemetry.addData("X (Meters): ",pose.getPosition().x);
+                telemetry.addData("Y (Meters): ",pose.getPosition().y);
+                telemetry.addData("Z (Meters): ", pose.getPosition().z);
+                telemetry.addData("Yaw (Degrees): ", Math.toDegrees(pose.getOrientation().getYaw()));
+                telemetry.addData("Pitch (Degrees): ", Math.toDegrees(pose.getOrientation().getPitch()));
+                telemetry.addData("Roll (Degrees): ", Math.toDegrees(pose.getOrientation().getRoll()));
             }
             else
                 telemetry.addLine("No tags detected.");
         }
         else
             telemetry.addLine("No valid result.");
-    }
-}
+
+ */
